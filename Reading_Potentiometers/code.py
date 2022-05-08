50d31fb5aaa2cb183a959c0c1f77563bdf592380
Nicholas Ross <RossNicholasV@gmail.com>
2022-05-08 18:28:18 -0500

    Reading Second Potentiometer
    Reading and outputting info from second potentiometer. Rounding voltage values from potentiometers to make it easier to read on display.

--- Reading_Potentiometers/code.py
+++ Reading_Potentiometers/code.py
@@ -1,16 +1,21 @@
 import time
+import math
 import board
 import analogio
 import hd44780
 
 potentiometer = analogio.AnalogIn(board.GP26)
+potentiometer2 = analogio.AnalogIn(board.GP27)
 
 get_voltage = 3.3 / 65535
 
 display = hd44780.HD44780()
 
 while True:
-	voltage = str(potentiometer.value * get_voltage)
-	print(voltage)
-	display.write(voltage, 1)
-	time.sleep(2)
+	voltage = str(math.ceil((potentiometer.value * get_voltage) * 100) / 100)
+	voltage2 = str(math.ceil((potentiometer2.value * get_voltage) * 100) / 100)
+	print("X " + voltage)
+	display.write("X " + voltage, 1)
+	print("Y " + voltage2)
+	display.write("Y " + voltage2, 2)
+	time.sleep(1)
