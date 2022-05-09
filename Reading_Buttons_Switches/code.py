6f9beeaead8a268fb64cebb5924a9c6dde8e1060
Nicholas Ross <RossNicholasV@gmail.com>
2022-05-08 19:27:49 -0500

    Reading Button
    added reading buttons and displaying their value

--- Reading_Buttons_Switches/code.py
+++ Reading_Buttons_Switches/code.py
@@ -5,11 +5,15 @@ import hd44780
 
 button = digitalio.DigitalInOut(board.GP2)
 button.switch_to_input(pull=digitalio.Pull.DOWN)
+switch = digitalio.DigitalInOut(board.GP3)
+switch.switch_to_input(pull=digitalio.Pull.DOWN)
 
 display = hd44780.HD44780()
 
 while True:
 	display.clear()
-	print(button.value)
+	print("Button: " + str(button.value))
+	print("Switch: " + str(switch.value))
 	display.write("Button: " + str(button.value), 1)
+	display.write("Switch: " + str(switch.value), 2)
 	time.sleep(1)
